27a93a80204a70a161e8dde5d170dd1e8f32754e
Ryan Michael <kerinin@gmail.com>
2013-04-02 20:08:05 +0000

    fixes from linux box

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-echo "\nSETTING UP ALL THE THINGS!!!"
+echo "SETTING UP ALL THE THINGS!!!"
 echo "SSH keys should already be installed in .ssh (if you're going to do that)"
 
 command_exists () {
@@ -24,12 +24,12 @@ symlink_or_backup () {
 
 install_stuff () {
   if ! command_exists $1; then
-    read -p "Install $1?" yn
-    # case $yn in
-    #   [Yy]* ) eval $2
-    #   [Nn]* ) echo "Not installing $1"
-    #   * ) echo "Please answer Y or N"
-    # esac
+    echo -n "Install $1?" >&2
+    read yn
+    case $yn in
+      [Yy] ) echo "Installing $1"; eval $2;;
+      * ) echo "Not installing $1";;
+    esac
   else
     echo "$1 already installed"
   fi
@@ -37,8 +37,8 @@ install_stuff () {
 
 cd
 
-echo "\n---> Installing software:"
-if [ -n $INSTALL ] && [ $OSTYPE=~darwin ]; then
+echo "---> Installing software:"
+if [ -n "$INSTALL" ] && echo $OSTYPE | grep -Fq darwin; then
   install_stuff brew 'ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"'
   install_stuff git 'brew install git'
   install_stuff mysql 'brew install mysql'
@@ -48,7 +48,7 @@ if [ -n $INSTALL ] && [ $OSTYPE=~darwin ]; then
   install_stuff ruby-build 'brew install ruby-build'
   install_stuff wget 'brew install wget'
   install_stuff ag 'brew install the_silver_searcher'
-elif [ -n $INSTALL ] && [ $OSTYPE=~linux ]; then
+elif [ -n "$INSTALL" ] && echo $OSTYPE | grep -Fq linux; then
   install_stuff git 'sudo apt-get install git'
   install_stuff mysql 'sudo apt-get install msyql'
   install_stuff redis 'sudo apt-get install redis'
@@ -56,7 +56,8 @@ elif [ -n $INSTALL ] && [ $OSTYPE=~linux ]; then
   install_stuff rbenv 'sudo apt-get install rbenv'
   install_stuff ruby-build 'sudo apt-get install ruby-build'
   install_stuff wget 'sudo apt-get  install wget'
-  install_stuff ag 'apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev; git clone git://github.com/ggreer/the_silver_searcher.git; cd .the_silver_searcher; ./build.sh; sudo make install; cd'
+  install_stuff ag 'sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev; git clone git://github.com/ggreer/the_silver_searcher.git; cd .the_silver_searcher; ./build.sh; sudo make install; cd'
+  install_stuff zsh 'sudo apt-get install zsh'
 else
   echo "Either I don't recognize your OS ($OSTYPE) or you didn't set \$INSTALL=Y - not trying to install stuff"
 fi
@@ -66,7 +67,7 @@ if ! command_exists git; then
   return
 fi
 
-echo "\n---> Cloning environment repo:"
+echo "---> Cloning environment repo:"
 if ! [ -d environment ]; then
   if git clone git@github.com:kerinin/environment.git; then
     echo "Cloned environment repo with commit access"
@@ -81,31 +82,47 @@ else
   cd ../
 fi
 
-echo "\n--> Setting up git submodules:"
+echo "---> Cloning zsh repo:"
+if ! [ -d ~/.oh_my_zsh ]; then
+  if git clone git@github.com:kerinin/oh-my-zsh.git ~/.oh_my_zsh; then
+    echo "Cloned oh_my_zsh repo WITH commit access"
+  else
+    git clone git://github.com/kerinin/oh-my-zsh.git ~/.oh_my_zsh
+    echo "Clone oh_my_zsh WITHOUT commit access"
+  fi
+else
+  echo "oh_my_zsh already cloned, fetching master"
+  cd .oh_my_zsh
+  git pull
+  cd ../
+fi
+
+echo "--> Setting up git submodules:"
 cd environment
 git submodule init
 git submodule update
 cd
 
-echo "\n---> Symlinking environment:"
+echo "---> Symlinking environment:"
 if command_exists byobu; then symlink_or_backup .byobu environment/.byobu; fi
 if command_exists gem; then symlink_or_backup .gemrc environment/.gemrc; fi
 if command_exists rbenv; then symlink_or_backup .rbenv environment/.rbenv; fi
 if command_exists vim; then symlink_or_backup .vim environment/.vim; symlink_or_backup .vimrc environment/.vimrc; fi
 
-echo "\n---> Setting up init scripts:"
-if [ $OSTYPE=~darwin ] && ! grep -Fxq "source ~/environment/.profile_osx" ~/.zshrc; then
+echo "---> Setting up init scripts:"
+if echo $OSTYPE | grep -Fq darwin && ! grep -Fxq "source ~/environment/.profile_osx" ~/.zshrc; then
   echo "source ~/environment/.profile_osx" >> ~/.zshrc
-elif [ $OSTYPE=~linux ] && ! grep -Fxq "source ~/environment/.profile_ubuntu" ~/.zshrc; then
+elif echo $OSTYPE | grep -Fq linux && ! grep -Fxq "source ~/environment/.profile_profile" ~/.zshrc; then
   echo "source ~/environment/.profile_ubuntu" >> ~/.zshrc
 else
   echo "Either I don't recognize your OS ($OSTYPE) or your init scripts are already in place"
 fi
 
-echo "\n---> Setting login shell:"
-if [ $SHELL != /bin/zsh ]; then 
-  echo "Changing shell from $SHELL to /bin/zsh - you'll need to enter your password"
-  sudo chsh -s /bin/zsh $USER
+echo "---> Setting login shell:"
+if [ $SHELL!=/bin/zsh ]; then 
+  echo "Changing shell from $SHELL to /bin/zsh - you may need to enter your password"
+  sudo chsh -s /bin/zsh "$USER"
 else
   echo "login shell already zsh"
 fi
+
